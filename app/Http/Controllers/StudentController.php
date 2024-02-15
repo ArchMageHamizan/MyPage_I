6028a41e11a289408c8ee4b49f3158e98dffee25
HamizanYusni <hamizanyus000@gmail.com>
2024-02-15 09:04:21 +0800

    swap and changes from old to new local

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\student;
+
+class StudentController extends Controller
+{   
+
+    protected $student;
+    public function __construct(){
+        $this->student=new Student();
+    }
+
+    // 5 ACTION: 
+    //   @ DISPLAY LIST & SPECIFIC
+    //   @ STORE NEW
+    //   @ UPDATE SPECIFIC
+    //   @ DELETE SPECIFIC 
+    
+    // DISPLAY LIST of the resource.
+    public function index()
+    {
+        return $this->student->all();
+    }
+    // =============================
+      
+    // DISPLAY SPECIFIC resource.
+    public function show(string $id)
+    {
+        return $student=$this->student->find($id);
+    }
+    // =============================
+  
+    // STORE NEW created resource in storage.
+    public function store(Request $request)
+    {
+        return $this->student->create($request->all());
+    }
+    // =============================
+
+    
+    // UPDATE  SPECIFIC resource in storage.
+    public function update(Request $request, string $id)
+    {
+        $student=$this->student->find($id); // find the target student by refer to id
+                                            // assign the values to student object
+        $student->update($request->all());  // update the student 
+        return $student;
+    }
+    // =============================
+
+    // DELETE SPECIFIC resource from storage.
+    public function destroy(string $id)
+    {
+        $student =$this->$student->find($id);
+        return $student->delete();
+    }
+    // =============================
+}
